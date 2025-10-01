3f4633c5199a195bc96cda2d05b681f20010be93
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-10-01 17:04:53 +0300

    seminar 1

---
+++ Seminar1/main.py
@@ -0,0 +1,17 @@
+# liste
+temperaturi = [15, 22, -3, 0, 7]
+print("lista: ", temperaturi, type(temperaturi))
+print("primul element: ", temperaturi[0])
+print("ultimele doua elemente: ", temperaturi[-2:])
+
+# tupluri
+coordonate = (21, 45)
+# coordonate[0] = 33;
+
+# dictionare
+student = {"nume" : "Ana", "nota" : 10}
+print(student["nume"], student.get("nota"))
+
+# seturi
+litere = set("analiza datelor")
+print(litere)
