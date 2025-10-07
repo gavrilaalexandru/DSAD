f1e6d9b62b4ec03e0deb33afeac1d75b2e881e3a
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-10-07 12:06:14 +0300

    adaugat comentarii pt. seminarul 1

--- Seminar1/main.py
+++ Seminar1/main.py
@@ -1,17 +1,17 @@
 # liste
-temperaturi = [15, 22, -3, 0, 7]
+temperaturi = [15, 22, -3, 0, 7] # array normal
 print("lista: ", temperaturi, type(temperaturi))
 print("primul element: ", temperaturi[0])
 print("ultimele doua elemente: ", temperaturi[-2:])
 
 # tupluri
-coordonate = (21, 45)
+coordonate = (21, 45) # nu pot schimba valorile
 # coordonate[0] = 33;
 
 # dictionare
-student = {"nume" : "Ana", "nota" : 10}
+student = {"nume" : "Ana", "nota" : 10} # cheie valoare, cheia este unica
 print(student["nume"], student.get("nota"))
 
 # seturi
-litere = set("analiza datelor")
+litere = set("analiza datelor") # elimina duplicate si random
 print(litere)
